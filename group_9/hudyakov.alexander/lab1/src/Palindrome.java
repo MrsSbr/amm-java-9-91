d8b0a778d09ff88f74f48e57b0d42c74bc912cdf
KrRita <kravtsovarita2003@mail.ru>
2023-09-14 09:04:53 +0300

    add lab1 of alexander hudyakov

---
+++ group_9/hudyakov.alexander/lab1/src/Palindrome.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Palindrome {
+
+    private static  int reverse(int number){
+        int copy = number;
+        int reversed = 0;
+
+        while (copy != 0) {
+            int remainder = copy % 10;
+            reversed = reversed * 10 + remainder;
+            copy = copy / 10;
+        }
+        return reversed;
+    }
+
+    private static  boolean isPalindrome(int number){
+        return number == reverse(number);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input your number:");
+        try {
+            int number = scanner.nextInt();
+            System.out.println(isPalindrome(number));
+        } catch (InputMismatchException exception) {
+            System.out.println("Incorrect input");
+        }
+    }
+}
