7ddb8666d112e47171a9507e84f7d36d9c76b71b
KrRita <kravtsovarita2003@mail.ru>
2023-09-14 19:13:46 +0300

    add lab1 of kravtsova margarita

---
+++ group_9/kravtsova.margarita/lab1/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.HashSet;
+public class Main {
+    private static int [] fillArray(int n)
+    {
+        Scanner stream = new Scanner(System.in);
+        int [] newArray = new int[n];
+        System.out.println("Введите номера конфет: ");
+        int i = 0;
+        while(i < n) {
+         try {
+               newArray[i] = stream.nextInt();
+               if(Math.abs(newArray[i]) <= 100000)
+               i++;
+           }
+        catch (Exception ex){
+            System.out.println("Было введено неверное значение!\nПовторите ввод:\n");
+           }
+        }
+        return newArray;
+    }
+    private static int countTypesCandy(int[]candyType){
+        int countDifferentTypesCandies;
+        HashSet <Integer> variantsCandies = new HashSet<Integer>();
+        for(int i=0;i<candyType.length;i++) {
+            variantsCandies.add(candyType[i]);
+        }
+        return variantsCandies.size();
+    }
+
+    public static int maxCountTypes(int acceptableCount,int countTypes ) {
+        if (acceptableCount >= countTypes) {
+            return acceptableCount;
+
+        }else {
+            return countTypes;
+        }
+    }
+    public static void main(String[] args)
+    {
+        int n;
+        int [] candiesTypes;
+        System.out.println("Введите число n: ");
+        Scanner in = new Scanner(System.in);
+        boolean error = false;
+        do {
+            if(error)
+                System.out.println("Введено неверное значение!\nПовторите ввод:");
+            n = in.nextInt();
+            error = true;
+        } while(n < 2 || n > 10000 || n % 2 != 0);
+        candiesTypes = fillArray(n);
+        int acceptableCount = n/2;
+        int countTypes = countTypesCandy(candiesTypes);
+        System.out.println(String.format("Максимальное количество типов конфет: %d\n", maxCountTypes(acceptableCount,countTypes)));
+
+    }
+}
