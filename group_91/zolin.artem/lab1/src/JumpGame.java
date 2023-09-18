ecddef7a6034a596b8e1289d9f13fa52ba0aa733
zigzag <wwe.15@list.ru>
2023-09-18 17:34:39 +0300

    add lab1 of zolin artem

---
+++ group_91/zolin.artem/lab1/src/JumpGame.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JumpGame {
+
+    private static int findMinimumJumps(int[] numbers) {
+        int jumps = 0;
+        int farthestReachableIndex = 0;
+        int currentJumpMaxIndex = 0;
+
+        for (int i = 0; i < numbers.length - 1; ++i) {
+            if (i + numbers[i] > farthestReachableIndex) {
+                farthestReachableIndex = i + numbers[i];
+            }
+            if (i == currentJumpMaxIndex) {
+                ++jumps;
+                currentJumpMaxIndex = farthestReachableIndex;
+            }
+        }
+
+        return jumps;
+    }
+
+    private static int[] readArrayFromConsole() {
+        var scanner = new Scanner(System.in);
+        var numbers = scanner.nextLine().split("\\s+");
+        return Arrays.stream(numbers).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Введите массив: ");
+        int[] numbers = readArrayFromConsole();
+        int minimumJumps = findMinimumJumps(numbers);
+        System.out.println("Минимальное количество прыжков: " + minimumJumps);
+    }
+
+}
