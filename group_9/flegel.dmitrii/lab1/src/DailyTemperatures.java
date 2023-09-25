b63afb4718546a2abfd4407177c381de58dc7fd7
Dmitrii Flegel <dmitrii.flegel@yandex.ru>
2023-09-25 19:28:30 +0300

    add lab1 of dmitrii flegel

---
+++ group_9/flegel.dmitrii/lab1/src/DailyTemperatures.java
@@ -0,0 +1,46 @@
+package lab1.src;
+
+import java.util.Stack;
+import java.util.Scanner;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DailyTemperatures solution = new DailyTemperatures();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите количество дней: ");
+        int n = scanner.nextInt();
+        int[] temperatures = new int[n];
+
+        System.out.println("Введите температуры для каждого дня:");
+        for (int i = 0; i < n; i++) {
+            temperatures[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        int[] result = solution.dailyTemperatures(temperatures);
+
+        System.out.print("Результат: ");
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
