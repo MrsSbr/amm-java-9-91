337b7522701bfb5bd88bff1807bdc3a5cd643aa2
FaraneysX <faraneys.up@gmail.com>
2023-09-27 15:20:42 +0300

    add lab1 of denisov nikita

---
+++ group_9/denisov.nikita/lab1/src/HappyNumber.java
@@ -0,0 +1,80 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class HappyNumber {
+    private static int nextNumber(int number) {
+        int result = 0;
+        int digit;
+
+        while (number > 0) {
+            digit = number % 10;
+            result += digit * digit;
+            number /= 10;
+        }
+
+        return result;
+    }
+
+    public static boolean isHappy(int number) {
+        HashSet<Integer> numbers = new HashSet<>();
+
+        // Число не является счастливым, если в процессе работы алгоритма появляется бесконечный цикл.
+        // 2 -> 4 -> 16 -> 37 -> 58 -> 89 -> 145 -> 42 -> 20 -> 4 -> 16...
+        // Проверяем, содержится ли в последовательности число, которое мы получили в результате работы алгоритма.
+        while (number != 1 && !numbers.contains(number)) {
+            numbers.add(number);
+            number = nextNumber(number);
+        }
+
+        return number == 1;
+    }
+
+    public static void Task(Scanner scanner) {
+        System.out.print("Введите число: ");
+
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+
+            System.out.println("Является ли число " + number + " счастливым: " + isHappy(number));
+        } else {
+            System.out.println("Ошибка ввода. Попробуйте снова.");
+            scanner.next(); // Считываем некорректный ввод, чтобы очистить буфер.
+        }
+    }
+
+    public static void Menu() {
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+
+        do {
+            System.out.print("""
+                    ГЛАВНОЕ МЕНЮ
+
+                    [1]. Проверка числа (является ли оно счастливым)
+                    [0]. Завершить работу
+
+                    Выберите действие:\s""");
+            choice = scanner.next().charAt(0);
+
+            switch (choice) {
+                case '1': {
+                    Task(scanner);
+                    break;
+                }
+                case '0': {
+                    scanner.close();
+
+                    System.out.print("Работа программы завершена.");
+                    break;
+                }
+                default: {
+                    System.out.println("Ошибка ввода. Попробуйте снова.");
+                }
+            }
+        } while (choice != '0');
+    }
+
+    public static void main(String[] args) {
+        Menu();
+    }
+}
