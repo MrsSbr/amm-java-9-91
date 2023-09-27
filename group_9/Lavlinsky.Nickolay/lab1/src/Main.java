d5ecffe32bcfda93c193ed988f08186c45735831
Lavlinsky Nickolay <kritikui24@gmail.com>
2023-09-27 17:36:06 +0300

    Николай Лавлинский добавил Лабу №1

--- group_9/Lavlinsky.Nickolay/lab1/src/Main.java
+++ group_9/Lavlinsky.Nickolay/lab1/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Input size of array:");
         Scanner scan = new Scanner(System.in);
-        int[] arr=new int [scan.nextInt()];
+        int[] arr = new int[scan.nextInt()];
         HashSet set = new HashSet();
-        for (int i=0; i<arr.length;i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println("Input your number:");
-            arr[i]=scan.nextInt();
+            arr[i] = scan.nextInt();
         }
-        for (int i=0;i<arr.length;i++) {
-            if(set.contains(arr[i])) {
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(arr[i])) {
                 System.out.println("True");
                 return;
             }
