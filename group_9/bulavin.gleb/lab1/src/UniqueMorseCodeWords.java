b1752761c7a704287490b8fc1d2f40bf488eee55
Bulavin Gleb <gleb2003bulav@gmail.com>
2023-10-04 22:42:36 +0300

    add lab0 of gleb bulavin

---
+++ group_9/bulavin.gleb/lab1/src/UniqueMorseCodeWords.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class UniqueMorseCodeWords {
+    private final static String[] MORSE_CODE_LIST = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
+            "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    private static String morseCodeOfSymbol(final char symbol) {
+        return MORSE_CODE_LIST[symbol - 'a'];
+    }
+    private static String morseCodeOfWords(final String word) {
+        StringBuilder builder = new StringBuilder();
+        for (char symbol : word.toCharArray())
+            builder.append(morseCodeOfSymbol(symbol));
+        return builder.toString();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashSet<String> set = new HashSet<>();
+        System.out.println("Введите слово (пустая строка для завершения ввода):");
+        String word = scanner.nextLine();
+        while(!word.isEmpty()) {
+            set.add(morseCodeOfWords(word));
+            word = scanner.nextLine();
+        }
+        System.out.printf("Количество различных преобразований среди всех слов: %d", set.size());
+    }
+}
