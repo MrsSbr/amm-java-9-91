ffd053c5cd6721ca72ebeea3e2742982b0db91b9
FaraneysX <faraneys.up@gmail.com>
2023-10-04 19:55:03 +0300

    add lab2 of denisov nikita

---
+++ group_9/denisov.nikita/lab2/src/Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu() {
+        Vehicle[] vehicles = {
+                new Car("Audi", "A1", 100),
+                new Motorbike("BMW", "F850 GS"),
+        };
+        Scanner scanner = new Scanner(System.in);
+        int choice = -1;
+
+        do {
+            System.out.print("""
+                    ГЛАВНОЕ МЕНЮ
+
+                    [1]. Работа с автомобилем
+                    [2]. Работа с мотоциклом
+                    [0]. Завершить работу
+                    Введите действие:\s""");
+
+            if (scanner.hasNextInt()) {
+                choice = scanner.nextInt();
+            }
+
+            switch (choice) {
+                case 1, 2 -> print(vehicles[choice - 1]);
+                case 0 -> {
+                    scanner.close();
+                    System.out.print("Работа программы завершена.");
+                }
+                default ->  {
+                    System.out.println("Ошибка ввода. Попробуйте снова.");
+                    scanner.nextLine();
+                }
+            }
+        } while (choice != 0);
+    }
+
+    public static void print(VehicleInterface vehicle) {
+        System.out.println();
+
+        vehicle.start();
+        vehicle.stop();
+
+        System.out.println();
+    }
+}
---
+++ group_9/denisov.nikita/lab2/src/Vehicle.java
@@ -0,0 +1,40 @@
+import java.util.Objects;
+
+public abstract class Vehicle implements VehicleInterface {
+    private final String brand;
+    private final String model;
+
+    Vehicle(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    @Override
+    public void start() {
+        System.out.print(this + " начало движение.");
+    }
+
+    @Override
+    public void stop() {
+        System.out.print(this + " остановилось.");
+    }
+
+    @Override
+    public String toString() {
+        return "Транспортное средство " + brand + ' ' + model;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof Vehicle vehicle)) {
+            return false;
+        }
+
+        return Objects.equals(brand, vehicle.brand) &&
+               Objects.equals(model, vehicle.model);
+    }
+}
