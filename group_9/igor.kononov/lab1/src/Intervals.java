f0fd4237bd49c5af07f1d417f9d434059b562f07
MYP4 <kononovigor2003@gmail.com>
2023-10-04 21:17:21 +0300

    add lab1 of igor kononov

--- group_9/igor.kononov/lab1/src/Intervals.java
+++ group_9/igor.kononov/lab1/src/Intervals.java
@@ -1,98 +1,75 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Intervals {
-    private static final int MIN_VALUE = 0;
-    private static final int MAX_VALUE = 10000;
+    public static int[] enterIntervals() {
 
-    /**
-     *  Initializes an array of intervals according to the user's choice
-     * @return initialized array of intervals
-     */
-    private static int[][] initIntervals() {
-        System.out.print("Enter the number of intervals: ");
-        var scanner = new Scanner(System.in);
-        var numberOfIntervals = scanner.nextInt();
-
-        if (numberOfIntervals < MIN_VALUE || numberOfIntervals > MAX_VALUE) {
-            return null;
-        }
-        return new int[numberOfIntervals][2];
-    }
-
-    /**
-     * Fills an array of intervals with values according to the user's choice
-     */
-    private static void enderIntervals(int[][] intervals) {
-        System.out.println("Enter intervals: ");
+        System.out.println("Введите интервалы: ");
         var scanner = new Scanner(System.in);
 
-        for (int i = 0; i < intervals.length; i++) {
-            System.out.print("Enter " + i + ": ");
-            intervals[i][0] = Math.abs(scanner.nextInt());
-            intervals[i][1] = Math.abs(scanner.nextInt());
-        }
-    }
+        String tempString = scanner.nextLine();
+        String[] numbers = tempString.split(", ");
 
-    /**
-     * Sorts an array of intervals by the first element
-     */
-    private static void sortIntervals(int[][] intervals) {
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = 0; j < intervals.length - i - 1; j++) {
-                if (intervals[j][0] > intervals[j + 1][0]) {
-                    int[] temp = intervals[j];
-                    intervals[j] = intervals[j + 1];
-                    intervals[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    /**
-     * Merges intersecting intervals
-     * @return merged intervals
-     */
-    private static int[][] mergeIntervals(int[][] intervals) {
-        var mergedIntervals = new ArrayList<int[]>();
-        var currentInterval = intervals[0];
-
-        for (int i = 1; i < intervals.length; i++) {
-            var nextInterval = intervals[i];
-
-            if (currentInterval[1] >= nextInterval[0]) {
-                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
-            } else {
-                mergedIntervals.add(currentInterval);
-                currentInterval = nextInterval;
-            }
-        }
-        mergedIntervals.add(currentInterval);
-
-        int[][] result = new int[mergedIntervals.size()][2];
-        for (int i = 0; i < mergedIntervals.size(); i++) {
-            result[i] = mergedIntervals.get(i);
+        var result = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++) {
+            result[i] = Integer.parseInt(numbers[i]);
         }
         return result;
     }
 
-    /**
-     * Prints intervals
-     */
-    private static void printIntervals(int[][] intervals) {
-        System.out.println("Intervals: ");
-        for (int[] interval : intervals) {
-            System.out.println("[" + interval[0] + ", " + interval[1] + "]");
+    public static String mergeIntervals(int[] intervals) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        int a = intervals[0];   // Первый элемент интервала
+        int b = intervals[1];   // Второй элемент интервала
+        int c = intervals[2];   // Первый элемент последующего интервала
+
+
+        int bIndex = 1;
+        int cIndex = 2;
+        while (bIndex < intervals.length - 1) {
+            if (c <= b) {
+                bIndex += 2;
+                b = intervals[bIndex];
+                cIndex += 2;
+                c = intervals[cIndex];
+            }
+            else {
+                stringBuilder.append("[");
+                stringBuilder.append(a);
+                stringBuilder.append(",");
+                stringBuilder.append(b);
+                stringBuilder.append("]");
+                stringBuilder.append(", ");
+                a = intervals[cIndex];
+
+                cIndex += 2;
+                bIndex += 2;
+
+                if (cIndex != intervals.length) {
+                    b = intervals[bIndex];
+                    c = intervals[cIndex];
+                }
+                else {
+                    stringBuilder.append("[");
+                    stringBuilder.append(a);
+                    stringBuilder.append(",");
+                    b = intervals[bIndex];
+                    stringBuilder.append(b);
+                    stringBuilder.append("]");
+                }
+            }
         }
+
+        stringBuilder.append("]");
+        return stringBuilder.toString();
     }
 
+    // 1, 2, 5, 6, 5, 10, 11, 15, 14, 20, 21, 22
+    // 1, 3, 2, 6, 8, 10, 15, 18
     public static void main(String[] args) {
-        var intervals = initIntervals();
-        if (intervals == null)
-            return;
-
-        enderIntervals(intervals);
-        sortIntervals(intervals);
-        printIntervals(mergeIntervals(intervals));
+        var intervals = enterIntervals();
+        System.out.println("Merged intervals:");
+        System.out.println(mergeIntervals(intervals));
     }
 }
