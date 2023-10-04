fa6effcc49138b40cc37863e0662464dfe20c82f
Dmitrii Flegel <dmitrii.flegel@yandex.ru>
2023-10-04 23:50:23 +0300

    add lab2 of dmitrii flegel

---
+++ group_9/flegel.dmitrii/lab2/src/Ankylosaurus.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Ankylosaurus extends Dinosaur implements HerbivorousDinosaur {
+
+    private final int armorThickness;
+
+    public Ankylosaurus(String name, int age, int armorThickness) {
+        super(name, age);
+        this.armorThickness = armorThickness;
+    }
+
+    public int getArmorThickness() {
+        return armorThickness;
+    }
+
+    @Override
+    public void roar() {
+        System.out.println("Анкилозавр " + getName() + " рычит.");
+    }
+
+    @Override
+    public void graze() {
+        System.out.println("Анкилозавр " + getName() + " пасется. " +
+                "Он в безопасности благодаря своей броне толщиной " + getArmorThickness() + " мм.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Ankylosaurus)) return false;
+        Ankylosaurus other = (Ankylosaurus) o;
+        return super.equals(o) && armorThickness == other.armorThickness;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), armorThickness);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Вид: Анкилозавр. Толщина брони: " + getArmorThickness() + " мм.";
+    }
+
+}
---
+++ group_9/flegel.dmitrii/lab2/src/Brachiosaurus.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Brachiosaurus extends Dinosaur implements HerbivorousDinosaur {
+
+    private final int neckLength;
+
+    public Brachiosaurus(String name, int age, int neckLength) {
+        super(name, age);
+        this.neckLength = neckLength;
+    }
+
+    public int getNeckLength() {
+        return neckLength;
+    }
+
+    @Override
+    public void roar() {
+        System.out.println("Брахиозавр " + getName() + " рычит.");
+    }
+
+    @Override
+    public void graze() {
+        System.out.println("Брахиозавр " + getName() + " пасется. " +
+                "Он добирается до верхушек деревьев благодаря шее длиной " + getNeckLength() + " м.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Brachiosaurus)) return false;
+        Brachiosaurus other = (Brachiosaurus) o;
+        return super.equals(o) && neckLength == other.neckLength;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), neckLength);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Вид: Брахиозавр. Длина шеи: " + getNeckLength() + " м.";
+    }
+    
+}
