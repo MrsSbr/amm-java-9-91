96bf961776eb4b7b070b2bacd08a1d1845c6d338
Kristina Nabatnikova <kristina040803@mail.ru>
2023-10-05 00:27:12 +0300

    add lab1 of nabatnikova kristina

---
+++ group_9/nabatnikova.kristina/lab1/src/PerfectSquare.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static boolean isPerfectValid(int num) {
+        int i = 1;
+        while (i <= num) {
+            if (i*i == num) {
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("Введите число:");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int num = scanner.nextInt();
+            if (num>0) {
+                System.out.println(isPerfectValid(num));
+            } else {
+                System.out.println("Некорректный ввод!");
+            }
+        } else {
+            System.out.println("Некорректный ввод!");
+        }
+    }
+}
