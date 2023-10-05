592cc31f6b3125e11f5209e6306ea45fe2c5e37b
MrsSbr <daria.sanna@gmail.com>
2023-10-05 09:03:38 +0300

    Added 2 task

--- group_9/bulavin.gleb/lab1/src/UniqueMorseCodeWords.java
+++ group_9/bulavin.gleb/lab1/src/UniqueMorseCodeWords.java
@@ -1,24 +1,29 @@
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class UniqueMorseCodeWords {
-    private final static String[] MORSE_CODE_LIST = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.",
-            "---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    private final static String[] MORSE_CODE_LIST = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+            "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+
     private static String morseCodeOfSymbol(final char symbol) {
         return MORSE_CODE_LIST[symbol - 'a'];
     }
+
     private static String morseCodeOfWords(final String word) {
         StringBuilder builder = new StringBuilder();
-        for (char symbol : word.toCharArray())
+        for (char symbol : word.toCharArray()) {
             builder.append(morseCodeOfSymbol(symbol));
+        }
         return builder.toString();
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        HashSet<String> set = new HashSet<>();
+        Set<String> set = new HashSet<>();
         System.out.println("Введите слово (пустая строка для завершения ввода):");
         String word = scanner.nextLine();
-        while(!word.isEmpty()) {
+        while (!word.isEmpty()) {
             set.add(morseCodeOfWords(word));
             word = scanner.nextLine();
         }
