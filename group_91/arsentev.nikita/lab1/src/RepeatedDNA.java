7e9d7437cf98344c302bc814180f8ba24a5400ec
Nikita Arsentev <necit717@gmail.com>
2023-10-05 21:31:21 +0300

    edit lab1 of nikita arsentev

--- group_91/arsentev.nikita/lab1/src/RepeatedDNA.java
+++ group_91/arsentev.nikita/lab1/src/RepeatedDNA.java
@@ -7,70 +7,70 @@ import java.util.Random;
 import java.util.InputMismatchException;
 
 public class RepeatedDNA {
-    private final static String  CHARS = "ACGT";
+    private final static String CHARS = "ACGT";
     private final static int LENGTH = 10;
     private final static int EXCEPTION_SIZE = 100000;
-    private static List<String> findRepeatedDNASequences(String s){
+
+    private static List<String> findRepeatedDNASequences(String s) {
         Map<String, Integer> mapStrings = new HashMap<>();
         final int end = s.length() - LENGTH;
-        for(int i = 0; i <= end; i++){
+        for (int i = 0; i <= end; i++) {
             String string = s.substring(i, i + LENGTH);
-            mapStrings.put(string, mapStrings.getOrDefault(string, 0) + 1);
+            mapStrings.merge(string, 1, Integer::sum);//upd: replaced put with merge
         }
         List<String> result = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry : mapStrings.entrySet()){
-            if(entry.getValue() > 1){
+        for (Map.Entry<String, Integer> entry : mapStrings.entrySet()) {
+            if (entry.getValue() > 1) {
                 result.add(entry.getKey());
             }
         }
         return result;
     }
-    private static StringBuilder fillString(int count){
+
+    private static String fillString(int count) {
         StringBuilder result = new StringBuilder(count);
-        final int[] array = new Random().ints(count, 0, CHARS.length() ).toArray();
-        for(int i = 0; i < count; i++){
-            result.insert(i, CHARS.charAt(array[i]));
+        Random random = new Random();
+        for (int i = 0; i < count; i++) {
+            result.insert(i, CHARS.charAt(random.nextInt(CHARS.length())));//upd:generate a number in each iteration
         }
-        return result;
+        return result.toString();//upd: return String
     }
 
-    public static void main(String[]args){
-        Scanner in = new Scanner(System.in);
+    private static String getString() {//upd: added a line enter function
         System.out.println("How to fill string\n0 - Console, other - Random");
-        try {
-            final int var = in.nextInt();
-            StringBuilder s = new StringBuilder();
-            if(var == 0){
-                System.out.println("Print string:");
-                String tmp = in.nextLine();
-                tmp = in.nextLine();
-                if(tmp.isEmpty() || tmp.length() > EXCEPTION_SIZE || !tmp.matches("[" + CHARS + "]+"))
-                    throw new InputMismatchException();
-                s.append(tmp);
+        Scanner in = new Scanner(System.in);
+        final int number = in.nextInt();//upd: change name of variable
+        String s = "";
+        if (number == 0) {
+            System.out.println("Print string:");
+            in.nextLine();
+            s = in.nextLine();
+            if (s.isEmpty() || s.length() > EXCEPTION_SIZE || !s.matches("[" + CHARS + "]+")) {
+                throw new InputMismatchException();
             }
-            else{
-                System.out.println("Enter length of string -> ");
-                final int size = in.nextInt();
-                if(size < 1 || size > EXCEPTION_SIZE)
-                    throw new InputMismatchException();
-                s = fillString(size);
+        } else {
+            System.out.println("Enter length of string -> ");
+            final int size = in.nextInt();
+            if (size < 1 || size > EXCEPTION_SIZE) {
+                throw new InputMismatchException();
             }
-            System.out.println("DNA Sequences:\n" + s);
-            List<String> result = findRepeatedDNASequences(s.toString());
-            if(!result.isEmpty()){
-                StringBuilder print = new StringBuilder();
-                print.append("[ ").append(result.get(0));
-                for(int i = 1; i < result.size();++i){
-                    print.append(", ").append(result.get(i));
-                }
-                print.append("]");
-                System.out.println("Result:\n" + result);
-            }
-            else
-                System.out.println("there are no repeated substrings");
+            s = fillString(size);
         }
-        catch (InputMismatchException exception){
-            System.out.println("Incorrect input");
+        return s;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String s = getString();
+            System.out.println("DNA Sequences:\n" + s);
+            List<String> result = findRepeatedDNASequences(s);
+            if (!result.isEmpty()) {
+                System.out.println("Result:\n" + result);//upd: change output of list
+            } else {
+                System.out.println("There are no repeated substrings");
+            }
+        } catch (InputMismatchException exception) {
+            System.out.println(exception.getMessage());//upd: add output exception
         }
     }
 }
