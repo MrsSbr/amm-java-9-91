f33dbe0ad02f599e5807294ca3760e79c80b92d7
Torgash0 <gaydaev1654@icloud.com>
2023-10-06 10:12:23 +0300

    add lab1 of dmitri gaydaev (more fast0

--- group_91/dmitri.gaydaev/lab1/src/Main.java
+++ group_91/dmitri.gaydaev/lab1/src/Main.java
@@ -1,37 +1,3 @@
-//public class Main {
-//    public static void main(String[] args) {
-//        int[] prices = {7, 1, 5, 3, 6, 4};
-//        int maxProfit = maxProfit(prices);
-//        System.out.println("Максимальная прибыль: " + maxProfit);
-//    }
-//
-//    public static int maxProfit(int[] prices) {
-//        return calculateMaxProfit(prices, 0);
-//    }
-//
-//    public static int calculateMaxProfit(int[] prices, int start) {
-//        if (start >= prices.length) {
-//            return 0;
-//        }
-//
-//        int maxProfit = 0;
-//
-//        for (int i = start; i < prices.length; i++) {
-//            for (int j = i + 1; j < prices.length; j++) {
-//                if (prices[j] > prices[i]) {
-//                    int profit = prices[j] - prices[i] + calculateMaxProfit(prices, j + 1);
-//                    if (profit > maxProfit) {
-//                        maxProfit = profit;
-//                    }
-//                }
-//            }
-//        }
-//
-//        return maxProfit;
-//    }
-//}
-//
-//
 
 
 public class Main {
