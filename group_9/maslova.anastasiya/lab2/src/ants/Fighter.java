3d772d6266e5716ce657e20a3ca9a3140d3009ae
AnastasiaMaslova1 <dab578@mail.ru>
2023-10-18 12:37:14 +0300

    add lab2 of anastasiya maslova

--- group_9/maslova.anastasiya/lab2/src/ants/Fighter.java
+++ group_9/maslova.anastasiya/lab2/src/ants/Fighter.java
@@ -1,9 +1,8 @@
+package ants;
+
 /**
  * The Fighter interface provides a contract for entities that can engage in combat.
  */
 public interface Fighter {
-    /**
-     * Perform a fighting action.
-     */
     void fight();
 }
