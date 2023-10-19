251f0a49e470336555de20f1281d474b19ec8d0f
Егор Осинкин <osinkineg@yandex.ru>
2023-10-19 08:45:43 +0300

    add lab1 of egor osinkin

--- group_91/osinkin.egor/lab1/src/Permutation.java
+++ group_91/osinkin.egor/lab1/src/Permutation.java
@@ -10,7 +10,7 @@ public class Permutation {
             System.out.println("1 <= len <= 6");
             System.out.println("Input len: ");
             len = in.nextInt();
-        } while(len < 1 || len > 6);
+        } while (len < 1 || len > 6);
         Set<Integer> included = new HashSet<>();
         int[] array = new int[len];
         int i = 0;
@@ -18,35 +18,34 @@ public class Permutation {
         while (i < len) {
             System.out.printf("Input array[%d]: ", i);
             tmp = in.nextInt();
-            if (tmp <= 10 && tmp >= -10 && !included.contains(tmp)){
+            if (tmp <= 10 && tmp >= -10 && !included.contains(tmp)) {
                 included.add(tmp);
                 array[i] = tmp;
                 i++;
-            }
-            else{
+            } else {
                 System.out.println("Error, need unic -10<=elem<=10");
             }
         }
         getPermutations(array, len, len);
     }
-    public static void printArr(int[] a, int n)
-    {
+
+    public static void printArr(int[] a, int n) {
         for (int i = 0; i < n; i++)
             System.out.print(a[i] + " ");
         System.out.println();
     }
+
     public static void getPermutations(int[] nums, int size, int n) {
-        if (size == 1){
+        if (size == 1) {
             printArr(nums, n);
         }
-        for (int i=0;i<size;i++) {
-            getPermutations(nums, size-1, n);
+        for (int i = 0; i < size; i++) {
+            getPermutations(nums, size - 1, n);
             if (size % 2 == 1) {
                 int temp = nums[0];
                 nums[0] = nums[size - 1];
                 nums[size - 1] = temp;
-            }
-            else {
+            } else {
                 int temp = nums[i];
                 nums[i] = nums[size - 1];
                 nums[size - 1] = temp;
