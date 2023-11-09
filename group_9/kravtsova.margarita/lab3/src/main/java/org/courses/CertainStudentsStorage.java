350670e5c37ad2e717c9328666c3fb80ef8a11f1
KrRita <kravtsovarita2003@mail.ru>
2023-11-09 08:56:49 +0300

    changes

--- group_9/kravtsova.margarita/lab3/src/main/java/org/courses/CertainStudentsStorage.java
+++ group_9/kravtsova.margarita/lab3/src/main/java/org/courses/CertainStudentsStorage.java
@@ -5,13 +5,13 @@ import java.util.stream.Stream;
 
 public class CertainStudentsStorage implements StudentsStorage {
     private Collection<Student> certainStudents;
+    public CertainStudentsStorage(Collection<Student> certainStudents) {
+        this.certainStudents = certainStudents;
+    }
     public Collection<Student> getCertainStudents() {
         return certainStudents;
     }
     public void setCertainStudents(Collection<Student> certainStudents) {
-        this.certainStudents = certainStudents;
-    }
-    public CertainStudentsStorage(Collection<Student> certainStudents) {
         this.certainStudents = certainStudents;
     }
     @Override
