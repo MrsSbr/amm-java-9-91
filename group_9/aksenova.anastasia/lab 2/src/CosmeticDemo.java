d6891dcfee4855944a84dac50c083fd8a1838184
Nastya Axenova <strgeenaan2018@gmail.com>
2023-11-14 14:37:25 +0300

    change lab2 of aksenova anastasia

--- group_9/aksenova.anastasia/lab 2/src/CosmeticDemo.java
+++ group_9/aksenova.anastasia/lab 2/src/CosmeticDemo.java
@@ -9,25 +9,39 @@ public class CosmeticDemo {
         CosmeticProduct mascara1 = new Mascara("Водостойкая тушь", 8.99, true);
         CosmeticProduct mascara2 = new Mascara("Тушь без водостойкости", 7.99, false);
 
-        if (lipstick1.equals(lipstick2)) {
-            System.out.println("Первая и вторая помады равны.");
-        } else {
-            System.out.println("Первая и вторая помады не равны.");
+        // Сравнение объектов с использованием instanceof
+        if (lipstick1 instanceof Lipstick && lipstick2 instanceof Lipstick) {
+            Lipstick lip1 = (Lipstick) lipstick1;
+            Lipstick lip2 = (Lipstick) lipstick2;
+
+            if (lip1.equals(lip2)) {
+                System.out.println("Первая и вторая помады равны.");
+            } else {
+                System.out.println("Первая и вторая помады не равны.");
+            }
         }
 
-        if (mascara1.equals(mascara2)) {
-            System.out.println("Первая и вторая туши равны.");
-        } else {
-            System.out.println("Первая и вторая туши не равны.");
+        if (mascara1 instanceof Mascara && mascara2 instanceof Mascara) {
+            Mascara mas1 = (Mascara) mascara1;
+            Mascara mas2 = (Mascara) mascara2;
+
+            if (mas1.equals(mas2)) {
+                System.out.println("Первая и вторая туши равны.");
+            } else {
+                System.out.println("Первая и вторая туши не равны.");
+            }
         }
 
+        // Вывод информации о продуктах
         System.out.println(lipstick1);
         System.out.println(lipstick2);
         System.out.println(mascara1);
         System.out.println(mascara2);
 
-        System.out.println("Помада и тушь равны? " + lipstick1.deepEquals(mascara1));
+        // Сравнение объектов помады и туши с использованием метода deepEquals
+        System.out.println("Помада и тушь равны? " + lipstick1.equals(mascara1));
 
+        // Дополнительные примеры
         Lipstick redLipstick = new Lipstick("Красная помада", 10.99, "Красный");
         Mascara voluminousMascara = new Mascara("Объемная тушь", 9.99, false);
 
--- group_9/aksenova.anastasia/lab 2/src/makeup/CosmeticProduct.java
+++ group_9/aksenova.anastasia/lab 2/src/makeup/CosmeticProduct.java
@@ -26,14 +26,6 @@ public abstract class CosmeticProduct {
     }
 
 
-    public boolean deepEquals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        CosmeticProduct product = (CosmeticProduct) obj;
-        return Double.compare(product.price, price) == 0 &&
-                Objects.equals(name, product.name);
-    }
-
     @Override
     public int hashCode() {
         return Objects.hash(name, price);
