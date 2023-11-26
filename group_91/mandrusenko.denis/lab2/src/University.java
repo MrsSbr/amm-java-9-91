a07702a87cfeca12fa1911e2a114c748946d7852
Denis Mandrusenko <mandrucenko@gmail.com>
2023-11-26 19:44:59 +0300

    add lab2

--- group_91/mandrusenko.denis/lab2/src/University.java
+++ group_91/mandrusenko.denis/lab2/src/University.java
@@ -12,8 +12,9 @@ public class University {
         for (UniversityMember member : members) {
             if (member instanceof Student student) {
                 System.out.println(member.toString());
-            } else if (member instanceof Professor professor)
+            } else if (member instanceof Professor professor) {
                 System.out.println(member.toString());
+            }
             member.work();
             System.out.println();
         }
