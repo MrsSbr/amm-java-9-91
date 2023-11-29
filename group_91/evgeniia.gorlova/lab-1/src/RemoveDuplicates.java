d7657cc3b1c51cb4103b3b775847f2477bcfb5ce
ronno27 <gorlovaev04@gmail.com>
2023-11-30 00:45:13 +0300

    add lab-1 of evgeniia gorlova.

--- group_91/evgeniia.gorlova/lab-1/src/RemoveDuplicates.java
+++ group_91/evgeniia.gorlova/lab-1/src/RemoveDuplicates.java
@@ -5,7 +5,7 @@ public class RemoveDuplicates {
         }
         int k = 1;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != nums[i - 1]) {
+            if (!nums[i].equals(nums[i - 1])) {
                 nums[k] = nums[i];
                 k++;
             }
