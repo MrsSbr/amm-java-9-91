dd48d8efa131846e9be8402b15493745b1dec091
KrRita <kravtsovarita2003@mail.ru>
2023-12-12 01:20:32 +0300

    add changes

--- group_9/kravtsova.margarita/lab5/src/main/java/org/sql/queries/SqlQueryGenerator.java
+++ group_9/kravtsova.margarita/lab5/src/main/java/org/sql/queries/SqlQueryGenerator.java
@@ -2,6 +2,7 @@ package org.sql.queries;
 
 import org.sql.queries.annotations.TableSQL;
 import org.sql.queries.annotations.PrimaryKey;
+import org.sql.queries.exceptions.FieldAccessException;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -10,129 +11,145 @@ import java.util.stream.Collectors;
 
 public class SqlQueryGenerator {
     public String generateInsertQueries(Object entity) {
-        StringBuilder query = new StringBuilder();
         Class<?> typeClass = entity.getClass();
         List<Field> listFields = Arrays.stream(typeClass.getDeclaredFields()).toList();
         String fields = listFields.stream()
-                                  .filter(x->!x.isAnnotationPresent(PrimaryKey.class))
-                                  .map(Field::getName)
-                                  .collect(Collectors.joining(", "));
+                .filter(x -> !x.isAnnotationPresent(PrimaryKey.class))
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
         String values = listFields.stream()
-                                  .filter(field -> !field.isAnnotationPresent(PrimaryKey.class))
-                                  .peek(field -> field.setAccessible(true))
-                                  .map(field -> {
-                                      try {
-                                          return field.get(entity) != null ? field.get(entity).toString() : "NULL";
-                                      } catch (IllegalAccessException ex) {
-                                          throw new RuntimeException(ex);
-                                      }
-                                  })
-                                  .collect(Collectors.joining(", "));
+                .filter(field -> !field.isAnnotationPresent(PrimaryKey.class))
+                .peek(field -> field.setAccessible(true))
+                .map(field -> {
+                    try {
+                        return field.get(entity) != null ? convertField(field.get(entity)) : "NULL";
+                    } catch (IllegalAccessException ex) {
+                        throw new FieldAccessException("Невозможно получить доступ к private полю", field);
+                    }
+                })
+                .collect(Collectors.joining(", "));
         String nameTable = typeClass.getAnnotation(TableSQL.class)
-                                    .nameTable();
-        return query.append("INSERT INTO ")
-                    .append(nameTable)
-                    .append(" (")
-                    .append(fields)
-                    .append(")\nVALUES (")
-                    .append(values)
-                    .append(");")
-                    .toString();
+                .nameTable();
+        return "INSERT INTO " +
+                nameTable +
+                " (" +
+                fields +
+                ")\nVALUES (" +
+                values +
+                ");";
     }
 
     public String generateSelectAllEntitiesQuery(Class<?> typeClass) {
-        if(typeClass == null) {
-            throw new RuntimeException();
+        if (typeClass == null) {
+            throw new NullPointerException("Тип класса имел значение NULL");
         }
-        StringBuilder query = new StringBuilder();
         String nameTable = typeClass.getAnnotation(TableSQL.class)
-                                    .nameTable();
-        return query.append("SELECT *\nFROM ")
-                    .append(nameTable)
-                    .append(";")
-                    .toString();
+                .nameTable();
+        String fields = Arrays.stream(typeClass.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
+        return "SELECT " +
+                fields +
+                "\nFROM " +
+                nameTable +
+                ";";
     }
 
     public String generateSelectByPrimaryKeyEntityQuery(Class<?> typeClass, Integer pk) {
-        if(pk == null || typeClass == null) {
-            throw new RuntimeException();
+        if (pk == null) {
+            throw new NullPointerException("Первичный ключ равен NULL");
+        }
+        if (typeClass == null) {
+            throw new NullPointerException("Тип класса равен NULL");
         }
         Field primaryKey = Arrays.stream(typeClass.getDeclaredFields())
-                                 .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
-                                 .findFirst()
-                                 .get();
+                .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
+                .findFirst()
+                .orElseThrow(NullPointerException::new);
         String nameTable = typeClass.getAnnotation(TableSQL.class)
-                                    .nameTable();
-        StringBuilder query = new StringBuilder();
-        return query.append("SELECT *\nFROM ")
-                    .append(nameTable)
-                    .append("\nWHERE ")
-                    .append(primaryKey.getName())
-                    .append(" = ")
-                    .append(pk)
-                    .append(";")
-                    .toString();
+                .nameTable();
+        String fields = Arrays.stream(typeClass.getDeclaredFields())
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
+        return "SELECT " +
+                fields +
+                "\nFROM " +
+                nameTable +
+                "\nWHERE " +
+                primaryKey.getName() +
+                " = " +
+                pk +
+                ";";
     }
 
     public String generateUpdateEntitiesQuery(Object entity) {
-        StringBuilder query = new StringBuilder();
         List<Field> entityFields = Arrays.stream(entity.getClass().getDeclaredFields()).toList();
         Field primaryKey = entityFields.stream()
-                                       .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
-                                       .findFirst()
-                                       .get();
+                .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
+                .findFirst()
+                .orElseThrow(NullPointerException::new);
         primaryKey.setAccessible(true);
         String pk = "";
         try {
-            if(primaryKey.get(entity) == null) {
-                throw new RuntimeException();
+            if (primaryKey.get(entity) == null) {
+                throw new NullPointerException("Первичный ключ равен NULL");
             }
             pk = primaryKey.get(entity).toString();
-        } catch(IllegalAccessException ex) {
-            throw new RuntimeException();
+        } catch (IllegalAccessException ex) {
+            throw new FieldAccessException("Невозможно получить доступ к private полю", primaryKey);
         }
         String nameTable = entity.getClass()
-                                 .getAnnotation(TableSQL.class)
-                                 .nameTable();
+                .getAnnotation(TableSQL.class)
+                .nameTable();
         String fields = entityFields.stream()
-                                    .filter(field -> !field.isAnnotationPresent(PrimaryKey.class))
-                                    .peek(field -> field.setAccessible(true))
-                                    .map(field -> {
-                                        try {
-                                            return field.getName() + " = " + field.get(entity);
-                                        } catch (IllegalAccessException ex) {
-                                            throw new RuntimeException(ex);
-                                        }
-                                    })
-                                    .collect(Collectors.joining(", "));
-        return query.append("UPDATE ")
-                    .append(nameTable)
-                    .append("\nSET ")
-                    .append(fields)
-                    .append("\nWHERE ")
-                    .append(primaryKey.getName())
-                    .append(" = ")
-                    .append(pk)
-                    .append(";")
-                    .toString();
+                .filter(field -> !field.isAnnotationPresent(PrimaryKey.class))
+                .peek(field -> field.setAccessible(true))
+                .map(field -> {
+                    try {
+                        return field.getName() + " = " + convertField(field.get(entity));
+                    } catch (IllegalAccessException ex) {
+                        throw new FieldAccessException("Невозможно получить доступ к private полю", field);
+                    }
+                })
+                .collect(Collectors.joining(", "));
+        return "UPDATE " +
+                nameTable +
+                "\nSET " +
+                fields +
+                "\nWHERE " +
+                primaryKey.getName() +
+                " = " +
+                pk +
+                ";";
     }
+
     public String generateDeleteByPrimaryKeyEntityQuery(Class<?> typeClass, Integer pk) {
-        if(pk == null || typeClass == null) {
-            throw new RuntimeException();
+        if (pk == null) {
+            throw new NullPointerException("Первичный ключ равен NULL");
         }
-        StringBuilder query = new StringBuilder();
+        if (typeClass == null) {
+            throw new NullPointerException("Тип класса равен NULL");
+        }
         Field primaryKey = Arrays.stream(typeClass.getDeclaredFields())
-                                 .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
-                                 .findFirst()
-                                 .get();
+                .filter(field -> field.isAnnotationPresent(PrimaryKey.class))
+                .findFirst()
+                .orElseThrow(NullPointerException::new);
         String tableName = typeClass.getAnnotation(TableSQL.class).nameTable();
-        return query.append("DELETE from ")
-                    .append(tableName)
-                    .append("\nWHERE ")
-                    .append(primaryKey.getName())
-                    .append(" = ")
-                    .append(pk)
-                    .append(";")
-                    .toString();
+        return "DELETE from " +
+                tableName +
+                "\nWHERE " +
+                primaryKey.getName() +
+                " = " +
+                pk +
+                ";";
+    }
+
+    private String convertField(Object field) {
+        if (field instanceof String stringField) {
+            stringField = stringField.replaceAll("'", "''");
+            return "'" + stringField + "'";
+        } else {
+            return field.toString();
+        }
     }
 }
--- group_9/kravtsova.margarita/lab5/src/main/java/org/sql/queries/annotations/TableSQL.java
+++ group_9/kravtsova.margarita/lab5/src/main/java/org/sql/queries/annotations/TableSQL.java
@@ -1,8 +1,10 @@
 package org.sql.queries.annotations;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.Retention;
+
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface TableSQL {
