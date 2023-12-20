0a9651c6ddf9207c8826671d38c6323f99de666f
Ursulaaaa15 <sergeevnaan2018@gmail.com>
2023-12-20 21:00:28 +0300

    change lab3 of aksenova anastasia

--- group_9/aksenova.anastasia/lab_3/src/test/java/org/example/ElectionTest.java
+++ group_9/aksenova.anastasia/lab_3/src/test/java/org/example/ElectionTest.java
@@ -1,25 +1,24 @@
-package test.java.org.example;
+package org.example;
 
-import main.java.org.example.PerformanceTester;
-import main.java.org.example.RepresentativeSelector;
-import main.java.org.example.VoteCounter;
-import main.java.org.example.VotingSimulator;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-class ElectionTest {
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ElectionTest {
 
     @Test
-    void testSimulateVotingResults() {
+    public void testSimulateVotingResults() {
         List<Integer> votes = VotingSimulator.simulateVotingResults(10);
         assertEquals(10, votes.size());
     }
 
     @Test
-    void testCountVotes() {
+    public void testCountVotes() {
         List<Integer> votes = Arrays.asList(1, 2, 3, 1, 2, 3, 1, 2, 3, 1);
         List<Integer> candidateVotes = VoteCounter.countVotes(votes);
         assertEquals(4, candidateVotes.get(0));
@@ -28,14 +27,14 @@ class ElectionTest {
     }
 
     @Test
-    void testSelectRepresentative() {
+    public void testSelectRepresentative() {
         List<Integer> candidateVotes = Arrays.asList(4, 3, 5, 2, 1, 0, 7, 6, 9, 8, 11, 10);
         int maxVotes = RepresentativeSelector.selectRepresentative(candidateVotes);
         assertEquals(11, maxVotes);
     }
 
     @Test
-    void testTestCollectionPerformance() {
+    public void testTestCollectionPerformance() {
         List<Integer> votes = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         assertDoesNotThrow(() -> PerformanceTester.testCollectionPerformance(votes));
     }
