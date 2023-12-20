75fb991f86a78408c53358ac8c25051468e9a526
Alexander Hudyakov <sasha151787512312@yandex.ru>
2023-12-20 19:37:56 +0300

    add lab 6 of hudyakov alexander

---
+++ group_9/hudyakov.alexander/lab6/src/main/java/org/example/Smokers.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Random;
+
+public class Smokers {
+    public static void main(String[] args) {
+        Random random = new Random();
+        Bartender bartender = new Bartender(random);
+        Smoker weedSmoker = new Smoker(bartender.getWeedSmokerSemaphore(), "травка", random);
+        Smoker paperSmoker = new Smoker(bartender.getPaperSmokerSemaphore(), "бумага", random);
+        Smoker matchesSmoker = new Smoker(bartender.getMatchesSmokerSemaphore(), "спички", random);
+        bartender.setWeedSmoker(weedSmoker);
+        bartender.setPaperSmoker(paperSmoker);
+        bartender.setMatchesSmoker(matchesSmoker);
+        weedSmoker.start();
+        paperSmoker.start();
+        matchesSmoker.start();
+        bartender.start();
+        try {
+            Thread.sleep(30000);
+            matchesSmoker.stopSmoking();
+            weedSmoker.stopSmoking();
+            paperSmoker.stopSmoking();
+            bartender.stopSmoking();
+            matchesSmoker.join();
+            weedSmoker.join();
+            paperSmoker.join();
+            bartender.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Все закончили курить");
+    }
+}
