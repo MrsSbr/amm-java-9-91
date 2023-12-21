ed9e44b948a87211c746d6dde646d76cfa48157c
Ursulaaaa15 <sergeevnaan2018@gmail.com>
2023-12-21 21:39:55 +0300

    change lab 4 of aksenova anastasia

---
+++ group_9/aksenova.anastasia/lab 4/src/main/java/main/FloristRecord.java
@@ -0,0 +1,17 @@
+package main;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FloristRecord {
+    private List<FlowerBouquet> records = new ArrayList<>();
+
+    public void addRecord(FlowerBouquet bouquet) {
+
+        records.add(bouquet);
+    }
+
+    public List<FlowerBouquet> getRecords() {
+
+        return records;
+    }
+}
---
+++ group_9/aksenova.anastasia/lab 4/src/main/java/main/FlowerBouquet.java
@@ -0,0 +1,41 @@
+package main;
+import java.util.List;
+
+public class FlowerBouquet {
+    private String date;
+    private String bouquetType;
+    private List<String> bouquetComposition;
+    private double cost;
+    private String deliveryType;
+
+    public FlowerBouquet(String date, String bouquetType, List<String> bouquetComposition, double cost, String deliveryType) {
+        this.date = date;
+        this.bouquetType = bouquetType;
+        this.bouquetComposition = bouquetComposition;
+        this.cost = cost;
+        this.deliveryType = deliveryType;
+    }
+
+    public String getDate() {
+
+        return date;
+    }
+
+    public String getBouquetType() {
+
+        return bouquetType;
+    }
+
+    public List<String> getBouquetComposition() {
+
+        return bouquetComposition;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getDeliveryType() {
+        return deliveryType;
+    }
+}
