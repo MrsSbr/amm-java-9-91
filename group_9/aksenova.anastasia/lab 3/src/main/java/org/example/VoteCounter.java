d6d26d19122b12ea34a8cd33ddeca0487eb38b6a
Ursulaaaa15 <sergeevnaan2018@gmail.com>
2023-12-21 20:53:37 +0300

    add lab 3 of aksenova anastasia

---
+++ group_9/aksenova.anastasia/lab 3/src/main/java/org/example/VoteCounter.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VoteCounter {
+
+    public static List<Integer> countVotes(List<Integer> votes) {
+        List<Integer> candidateVotes = new ArrayList<>(12);
+        for (int i = 0; i < 12; i++) {
+            candidateVotes.add(0);
+        }
+        for (Integer vote : votes) {
+            candidateVotes.set(vote - 1, candidateVotes.get(vote - 1) + 1) ;
+        }
+        return candidateVotes;
+    }
+}
