6008de2e66f65228dc95875ab6a203f3700ecc45
Lavlinsky Nickolay <kritikui24@gmail.com>
2023-12-22 18:01:40 +0300

    Николай Лавлинский добавил Лабу №5

--- group_9/Lavlinsky.Nickolay/lab4/src/test/java/org/example/TortureStatisticsTest.java
+++ group_9/Lavlinsky.Nickolay/lab4/src/test/java/org/example/TortureStatisticsTest.java
@@ -1,18 +1,20 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.*;
 
 import java.util.*;
 
 public class TortureStatisticsTest {
+    private final TortureStatistics tortureStatistics = new TortureStatistics();
 
     @Test
     public void testWithEmptyList() {
         List<StudentRecord> records = new ArrayList<>();
-        assertTrue(TortureStatistics.countResitsByTool(records).isEmpty());
-        assertTrue(TortureStatistics.calculateTotalTortureTime(records).isEmpty());
-        assertTrue(TortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
+        assertTrue(tortureStatistics.countResitsByTool(records).isEmpty());
+        assertTrue(tortureStatistics.calculateTotalTortureTime(records).isEmpty());
+        assertTrue(tortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
     }
 
     @Test
@@ -20,9 +22,9 @@ public class TortureStatisticsTest {
         List<StudentRecord> records = Collections.singletonList(
                 new StudentRecord("Иван", TortureTool.JAVA_CODE_CONVENTION, 30, false)
         );
-        assertEquals(0, TortureStatistics.countResitsByTool(records).size());
-        assertEquals(30, TortureStatistics.calculateTotalTortureTime(records).get("Иван"));
-        assertTrue(TortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
+        assertEquals(0, tortureStatistics.countResitsByTool(records).size());
+        assertEquals(30, tortureStatistics.calculateTotalTortureTime(records).get("Иван"));
+        assertTrue(tortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
     }
 
     @Test
@@ -32,10 +34,10 @@ public class TortureStatisticsTest {
                 new StudentRecord("Мария", TortureTool.GITHUB_BUG, 45, false),
                 new StudentRecord("Иван", TortureTool.CODE_ERROR, 60, false)
         );
-        assertEquals(1, TortureStatistics.countResitsByTool(records).size());
-        assertEquals(90, TortureStatistics.calculateTotalTortureTime(records).get("Иван"));
-        assertEquals(45, TortureStatistics.calculateTotalTortureTime(records).get("Мария"));
-        assertTrue(TortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
+        assertEquals(1, tortureStatistics.countResitsByTool(records).size());
+        assertEquals(90, tortureStatistics.calculateTotalTortureTime(records).get("Иван"));
+        assertEquals(45, tortureStatistics.calculateTotalTortureTime(records).get("Мария"));
+        assertTrue(tortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
     }
 
     @Test
@@ -44,15 +46,15 @@ public class TortureStatisticsTest {
                 new StudentRecord("Иван", TortureTool.JAVA_CODE_CONVENTION, 30, true),
                 new StudentRecord("Мария", TortureTool.GITHUB_BUG, 45, true)
         );
-        assertEquals(2, TortureStatistics.countResitsByTool(records).size());
-        assertTrue(TortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
+        assertEquals(2, tortureStatistics.countResitsByTool(records).size());
+        assertTrue(tortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
 
         records = Arrays.asList(
                 new StudentRecord("Иван", TortureTool.JAVA_CODE_CONVENTION, 30, false),
                 new StudentRecord("Мария", TortureTool.GITHUB_BUG, 45, false)
         );
-        assertTrue(TortureStatistics.countResitsByTool(records).isEmpty());
-        assertTrue(TortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
+        assertTrue(tortureStatistics.countResitsByTool(records).isEmpty());
+        assertTrue(tortureStatistics.studentsTorturedByAllToolsAndPassed(records).isEmpty());
     }
 
     @Test
@@ -61,7 +63,7 @@ public class TortureStatisticsTest {
         for (TortureTool tool : TortureTool.values()) {
             records.add(new StudentRecord("Иван", tool, 30, false));
         }
-        List<String> passedStudents = TortureStatistics.studentsTorturedByAllToolsAndPassed(records);
+        List<String> passedStudents = tortureStatistics.studentsTorturedByAllToolsAndPassed(records);
         assertEquals(1, passedStudents.size());
         assertTrue(passedStudents.contains("Иван"));
     }
