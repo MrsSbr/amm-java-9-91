06089043fcd75418e6af994f6ce5151dfec95257
f1tura <ivanikom2003@gmail.com>
2023-12-22 23:27:06 +0300

    fixes

--- group_9/makarov.ivan/lab4/src/main/java/org/example/plants/PlantSaleEntryReader.java
+++ group_9/makarov.ivan/lab4/src/main/java/org/example/plants/PlantSaleEntryReader.java
@@ -3,45 +3,35 @@ package org.example.plants;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PlantSaleEntryReader {
-    private static final Logger LOGGER = Logger.getLogger(PlantSaleEntryReader.class.getSimpleName());
+    private static final Logger logger = Logger.getLogger(PlantSaleEntryReader.class.getSimpleName());
 
     public static List<PlantSaleEntry> read(Path filePath) throws IOException {
-        try (Stream<String> lines = Files.lines(filePath)) {
-            return lines.map(PlantSaleEntryReader::readPlantSaleEntry)
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            LOGGER.severe("Ошибка чтения файла: " + e.getMessage());
-            throw e;
-        }
+        logger.info("Чтение файла.");
+
+        List<String> lines = Files.readAllLines(filePath);
+
+        return lines.stream()
+                .map(PlantSaleEntryReader::readPlantSaleEntry)
+                .collect(Collectors.toList());
+
     }
 
     private static PlantSaleEntry readPlantSaleEntry(String line) {
         String[] parts = line.split(";");
 
-        if (parts.length == 5) {
-            try {
-                String plantName = parts[0];
-                String plantType = parts[1];
-                String potSize = parts[2];
-                double price = Double.parseDouble(parts[3]);
-                LocalDate saleDate = LocalDate.parse(parts[4]);
 
-                return new PlantSaleEntry(plantName, plantType, potSize, price, saleDate.toString());
-            } catch (NumberFormatException | DateTimeException e) {
-                LOGGER.warning("Ошибка при парсинге данных: " + e.getMessage());
-                throw new IllegalArgumentException();
-            }
-        } else {
-            LOGGER.warning("Неверный формат данных: " + line);
-            throw new IllegalArgumentException();
-        }
+        String plantName = parts[0];
+        String plantType = parts[1];
+        String potSize = parts[2];
+        double price = Double.parseDouble(parts[3]);
+        LocalDate saleDate = LocalDate.parse(parts[4]);
+
+        return new PlantSaleEntry(plantName, plantType, potSize, price, saleDate.toString());
     }
 }
--- group_9/makarov.ivan/lab4/src/test/java/PlantSaleReaderTest.java
+++ group_9/makarov.ivan/lab4/src/test/java/PlantSaleReaderTest.java
@@ -1,25 +1,33 @@
-import org.example.plants.PlantSaleEntryReader;
 import org.example.plants.PlantSaleEntry;
-import org.junit.jupiter.api.Assertions;
+import org.example.plants.PlantSaleEntryReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
+
 public class PlantSaleReaderTest {
-    private static final Path ERROR_DATA_FILE = Path.of("src", "test", "java",  "invalid_plant_sales.txt");
+    private static final Path ERROR_DATA_FILE = Path.of("src", "test", "java", "invalid_plant_sales.txt");
+    private static final Path VALID_DATA_FILE = Path.of("src", "test", "java", "plants.txt");
+
+    @Test
+    void readFromValidFile() throws IOException {
+        List<PlantSaleEntry> plantSaleEntries = PlantSaleEntryReader.read(VALID_DATA_FILE);
+
+        assertNotNull(plantSaleEntries);
+        assertFalse(plantSaleEntries.isEmpty());
+        assertFalse(plantSaleEntries.contains(null));
+    }
 
     @Test
     void readFromInvalidFile() {
-        assertThrows(IllegalArgumentException.class, () -> PlantSaleEntryReader.read(ERROR_DATA_FILE));
+        assertThrows(Exception.class, () ->  PlantSaleEntryReader.read(ERROR_DATA_FILE));
     }
-
 
 
 }
