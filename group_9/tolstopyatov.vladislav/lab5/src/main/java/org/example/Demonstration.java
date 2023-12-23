12bccd6aa26f1d3b801ba35eaa3db186f2f90131
Vladislav Tolstopyatov <vladisl.tolstopyatov2015@yandex.ru>
2023-12-23 11:49:14 +0300

    add Demonstration

---
+++ group_9/tolstopyatov.vladislav/lab5/src/main/java/org/example/Demonstration.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import benchmark.Benchmark;
+import service.IService;
+import service.Service;
+import statistics.Statistics;
+
+/*
+Создать универсальный прокси, который будет для каждого метода в интерфейсе с аннотацией @Benchmarked
+считать время выполнения и записывать его консоль.
+
+Использование должно выглядеть примерно так:
+
+interface Service {...}
+class Benchmark {...}
+main(...) {
+    Service service = ...;
+    Service trackedService = Benchmark.track(service);
+    trackedService.do1();
+    trackedService.do2();
+    ...
+    Stat stat = Benchmark.getStat(trackedService);
+    stat.print(System.out);
+}
+* */
+
+public class Demonstration {
+    public static void main(String[] args) {
+        IService service = new Service();
+        IService trackedService = Benchmark.track(service);
+
+        trackedService.do1();
+        trackedService.do2();
+
+        Statistics statistics = Benchmark.getStatistics(trackedService);
+        statistics.print(System.out);
+    }
+}
